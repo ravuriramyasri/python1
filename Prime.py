d65ac939af0c039744fed8ae3d080bf6474ef748
ravuriramyasri <42571523+ravuriramyasri@users.noreply.github.com>
2018-09-06 11:56:20 +0530

    Update Prime.py

--- Prime.py
+++ Prime.py
@@ -1,11 +1,10 @@
-num = 407
+num = int(raw_input())
 if num > 1:
    for i in range(2,num):
        if (num % i) == 0:
-           print(num,"is not a prime number")
-           print(i,"times",num//i,"is",num)
+           print("no")
            break
-   else:
-       print(num,"is a prime number")
-else:    
-       print(num,"is not a prime number")
+       else:
+           print("yes")
+   else:    
+       print("no")
